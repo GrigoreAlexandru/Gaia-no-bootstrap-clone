8f2e13f0c04751b906219815ea4f9be8f15e8662
GrigoreAlexandru <obamafuego@gmail.com>
2017-07-28 19:24:19 +0300

    cleanup

--- assets/js/gaia.js
+++ assets/js/gaia.js
@@ -1,54 +1,48 @@
-$(window).scroll(function(){
-	var scrollAmount=$(this).scrollTop();
-	
-	//$('.header').css({
-		//'transform':'translate(0px, '+scrollAmount /2 +'%)'
-	//});
+$(window).scroll(function() {
+ var scrollAmount = $(this).scrollTop();
 
-    if (scrollAmount>=300){
-        $('.container').css({
-            'background-color':'#fff' ,
-            
-            'height':'3.5rem',
-            'border-bottom-color': '#ddd',
-             'border-bottom-width':'1px',
-             'border-bottom-style':'solid'
-        });
-        
-        
-        $('#nav-left,.nav-right').css({
-            'top':'.7rem',
-            'color':'#8A8A8A'
-    
-            
-        })
-        $('#btn-dl').css({
-            'color':'.#fff'
-    
-            
-        });
-    
+ if (scrollAmount >= 300) {
+  $('.container').css({
+   'background-color': '#fff',
 
-    }
-    
-    else{
-        $('.container').css({
-            'background-color':'' ,
-            
-            'height':'',
-            'border-bottom-color': '',
-             'border-bottom-width':'',
-             'border-bottom-style':''
-        });
-        $('#nav-left,.nav-right').css({
-            'top':'',
-            'color':''
-    
-            
-        })
+   'height': '3.5rem',
+   'border-bottom-color': '#ddd',
+   'border-bottom-width': '1px',
+   'border-bottom-style': 'solid'
+  });
 
-    
-    }
-    
-   
+
+  $('#nav-left,.nav-right').css({
+   'top': '.7rem',
+   'color': '#8A8A8A'
+
+
+  })
+  $('#btn-dl').css({
+   'color': '.#fff'
+
+
+  });
+
+
+ } else {
+  $('.container').css({
+   'background-color': '',
+
+   'height': '',
+   'border-bottom-color': '',
+   'border-bottom-width': '',
+   'border-bottom-style': ''
+  });
+  $('#nav-left,.nav-right').css({
+   'top': '',
+   'color': ''
+
+
+  })
+
+
+ }
+
+
 });
